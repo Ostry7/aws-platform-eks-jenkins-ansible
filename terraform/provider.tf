2100377aede81c4eae9dcfd0093c88a480391905
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-07-06 14:36:38 +0200

    add aws provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  region = eu-north-1     # Europe(Stockholm)
-  profile = roboticusr    #IAM created 
+  region  = "eu-north-1" # Europe(Stockholm)
+  profile = "roboticusr" #IAM created 
 }
