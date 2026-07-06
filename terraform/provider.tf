116451246cb68151b3a0f0a8046b97cde5fb43af
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-07-06 18:49:30 +0200

    add tfstate to s3 bucket

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,9 +5,16 @@ terraform {
       version = "6.52.0"
     }
   }
+  backend "s3" {
+    bucket         = "jenkins-tfstate-ostry7"
+    key            = "jenkins/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
-  region  = "eu-north-1" # Europe(Stockholm)
+  region = "eu-north-1" # Europe(Stockholm)
   #profile = "roboticusr" #IAM created 
 }
