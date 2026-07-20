5f19ff95ecbcfc02cf8576828056c8f09a860870
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-07-20 13:32:06 +0200

    add terraform-infra dir

---
+++ terraform-infra/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.52.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "jenkins-tfstate-ostry7"
+    key            = "infra/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1" # Europe(Stockholm)
+  #profile = "roboticusr" #IAM created 
+}
